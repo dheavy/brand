f22816e1b06308aba5128e9d1047c7aac2b4ad4c
Davy Peter Braun <hello@davybraun.com>
2015-10-19 01:10:47 +0200

    Set up basic element on page, including texts, nav, form.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!');
+$(() => {
+  'use strict';
+
+  let $window = $(window),
+      $content = $('.mp-content');
+
+  function resize() {
+    let windowHeight = $window.height(),
+        contentHeight = $content.height(),
+        yPos = (windowHeight / 2 - contentHeight / 2);
+
+    $content.offset({ top: yPos });
+  }
+
+  $window.resize(resize);
+  resize();
+
+  $content.css('opacity', 1);
+});
