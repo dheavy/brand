e3fbcfe45a6ad5790f76d2b67d2c2db47109757e
Davy Peter Braun <hello@davybraun.com>
2015-10-30 23:25:06 +0100

    Generate poster from randomized list. Fallback to poster on mobile.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,30 @@
 $(function () {
 
   var $window = $(window),
-      $content = $('.mp-content');
+      $body = $('body'),
+      $content = $('.mp-content'),
+      $video = $('#mp-background');
+
+  function generatePosterUrl() {
+    var baseUrl = 'images/mp-poster-',
+        fileFormat = '.png',
+        maxIndex = 5;
+
+    return baseUrl
+           + (Math.floor(Math.random() * (maxIndex - 0 + 1) + 0)).toString()
+           + fileFormat;
+  }
+
+  function setPoster(url) {
+    $video.attr('poster', url);
+  }
+
+  function setVideo() {
+    $video.append(
+      '<source src="videos/mp.mp4" type="video/mp4">'
+      + '<source src="videos/mp.webm" type="video/webm">'
+    );
+  }
 
   function resize() {
     var windowHeight = $window.height(),
@@ -11,6 +34,12 @@ $(function () {
         yPos = (windowHeight / 2 - contentHeight / 2);
 
     $content.offset({ top: yPos });
+  }
+
+  setPoster(generatePosterUrl());
+
+  if (!$('html').hasClass('mobile')) {
+    setVideo();
   }
 
   $window.resize(resize);
