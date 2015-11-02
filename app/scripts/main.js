d01ac21e0d00d4a965f8d2a6acd22eba65a9cd22
Davy Peter Braun <hello@davybraun.com>
2015-11-02 22:36:12 +0100

    Fix JS after linting.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
+/*global mixpanel, $*/
+
 'use strict';
 
 $(function () {
 
   var $window = $(window),
-      $body = $('body'),
       $content = $('.mp-content'),
       $video = $('#mp-background'),
       $form = $('#mp-form');
@@ -16,6 +17,10 @@ $(function () {
     return baseUrl
            + (Math.floor(Math.random() * (maxIndex - 0 + 1) + 0)).toString()
            + fileFormat;
+  }
+
+  function isValidEmail(email) {
+    return email && email.length > 1 && email.indexOf('@') > 1;
   }
 
   function setPoster(url) {
@@ -37,47 +42,45 @@ $(function () {
     $content.offset({ top: yPos });
   }
 
-  function ajaxForm($form, $resultPlaceholder) {
-    $form.submit(function (e) {
+  function ajaxForm($f, $resultPlaceholder) {
+    $f.submit(function (e) {
       e.preventDefault();
 
-      $form.find('button').addClass('')
-
-      if (!isValidEmail($form.find('input[type="email"]').val())) {
-        $resultPlaceholder.html("Oops.. please enter a valid email.");
+      if (!isValidEmail($f.find('input[type="email"]').val())) {
+        $resultPlaceholder.html('Oops.. please enter a valid email.');
       } else {
         $resultPlaceholder.html('Just a sec...');
-        submitForm($form, $resultPlaceholder);
+        submitForm($f, $resultPlaceholder);
       }
     });
   }
 
-  function submitForm($form, $resultPlaceholder) {
+  function submitForm($f, $resultPlaceholder) {
     $.ajax({
       type: 'GET',
-      url: $form.attr('action'),
-      data: $form.serialize(),
+      url: $f.attr('action'),
+      data: $f.serialize(),
       cache: false,
       dataType: 'jsonp',
       jsonp: 'c', // Makes Mailchimp trigger a JSONP response
       contentType: 'application/json; charset=utf-8'
     })
     .success(function (data) {
-      if (data.result != 'success') {
-        var msg = data.msg || 'Sorry... something went wrong on our end. Please try again.';
+      if (data.result !== 'success') {
+        var msg = data.msg || 'Sorry... something went wrong on our end. Please try again.';
 
         if (data.result && data.result === 'error') {
-          msg = "Please enter a valid email address.";
+          msg = 'Please enter a valid email address.';
           toggleFormState(true);
         }
 
         if (data.msg && data.msg.indexOf('already subscribed') >= 0) {
-          msg = "You're already in the list. Thank you!";
+          msg = 'You\'re already in the list. Thank you!';
         }
 
         $resultPlaceholder.html(msg);
       } else {
-        $resultPlaceholder.html("Thank you!<br>We've sent a confirmation in your inbox.");
+        $resultPlaceholder.html('Thank you!<br>We\'ve sent a confirmation in your inbox.');
         hideForm();
       }
     })
@@ -96,11 +99,7 @@ $(function () {
 
   function hideForm() {
     $form.fadeOut();
-    mixpanel.track('User clicked to join Beta list.')
-  }
-
-  function isValidEmail(email) {
-    return email && email.length > 1 && email.indexOf('@') > 1;
+    mixpanel.track('User clicked to join Beta list.');
   }
 
   setPoster(generatePosterUrl());
