0142be8ecae6e6e1c67c4b25e29ee2e6c1f3b98c
Davy Peter Braun <hello@davybraun.com>
2015-11-02 22:18:08 +0100

    Implement Mailchimp.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,8 @@ $(function () {
   var $window = $(window),
       $body = $('body'),
       $content = $('.mp-content'),
-      $video = $('#mp-background');
+      $video = $('#mp-background'),
+      $form = $('#mp-form');
 
   function generatePosterUrl() {
     var baseUrl = 'images/mp-poster-',
@@ -36,6 +37,71 @@ $(function () {
     $content.offset({ top: yPos });
   }
 
+  function ajaxForm($form, $resultPlaceholder) {
+    $form.submit(function (e) {
+      e.preventDefault();
+
+      $form.find('button').addClass('')
+
+      if (!isValidEmail($form.find('input[type="email"]').val())) {
+        $resultPlaceholder.html("Oops.. please enter a valid email.");
+      } else {
+        $resultPlaceholder.html('Just a sec...');
+        submitForm($form, $resultPlaceholder);
+      }
+    });
+  }
+
+  function submitForm($form, $resultPlaceholder) {
+    $.ajax({
+      type: 'GET',
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache: false,
+      dataType: 'jsonp',
+      jsonp: 'c', // Makes Mailchimp trigger a JSONP response
+      contentType: 'application/json; charset=utf-8'
+    })
+    .success(function (data) {
+      if (data.result != 'success') {
+        var msg = data.msg || 'Sorry... something went wrong on our end. Please try again.';
+
+        if (data.result && data.result === 'error') {
+          msg = "Please enter a valid email address.";
+          toggleFormState(true);
+        }
+
+        if (data.msg && data.msg.indexOf('already subscribed') >= 0) {
+          msg = "You're already in the list. Thank you!";
+        }
+
+        $resultPlaceholder.html(msg);
+      } else {
+        $resultPlaceholder.html("Thank you!<br>We've sent a confirmation in your inbox.");
+        hideForm();
+      }
+    })
+    .fail(function (error) {
+      console.error('[MPLSR] Something went wrong...');
+      console.error(error);
+    });
+  }
+
+  function toggleFormState(setActive) {
+    var isButtonDisabled = !setActive,
+        buttonLabel = isButtonDisabled ? 'Hold on...' : 'Reserve my account';
+
+    $('#submit').prop('disabled', isButtonDisabled).text(buttonLabel);
+  }
+
+  function hideForm() {
+    $form.fadeOut();
+  }
+
+  function isValidEmail(email) {
+    return email && email.length > 1 && email.indexOf('@') > 1;
+  }
+
   setPoster(generatePosterUrl());
 
   if (!$('html').hasClass('mobile')) {
@@ -46,4 +112,6 @@ $(function () {
   resize();
 
   $content.css('opacity', 1);
+
+  ajaxForm($form, $('.mp-msg-info'));
 });
