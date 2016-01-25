7020fc257d3fb89f09e476023d1f7225ecceda93
Davy Peter Braun <hello@davybraun.com>
2016-01-25 18:00:58 +0100

    Change ProvidersScreen comp from class to stateless function.

--- src/components/ProvidersScreen.jsx
+++ src/components/ProvidersScreen.jsx
@@ -1,17 +1,24 @@
+/**
+ * MyPleasure | "brand"
+ * --------------------
+ * "Providers" screen.
+ */
+
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import PureComponent from './PureComponent';
 import React, {PropTypes} from 'react';
 
-export default class ProvidersScreen extends PureComponent {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
-    providersNormal: ImmutablePropTypes.list.isRequired,
-    providersNaughty: ImmutablePropTypes.list.isRequired,
-    cta: PropTypes.string.isRequired
-  };
+const ProvidersScreen = ({title, subtitle, providersNormal, providersNaughty, cta}) => {
+  return (
+    <div>ProvidersScreen</div>
+  );
+};
 
-  render() {
-    return <div>ProvidersScreen</div>;
-  }
-}
+ProvidersScreen.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  providersNormal: ImmutablePropTypes.list.isRequired,
+  providersNaughty: ImmutablePropTypes.list.isRequired,
+  cta: PropTypes.string.isRequired
+};
+
+export default ProvidersScreen;
