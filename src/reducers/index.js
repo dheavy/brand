ca345776c2aaba4c67fd78ea6bfbc750047f731f
Davy Peter Braun <hello@davybraun.com>
2016-01-25 13:54:05 +0100

    Add store and reducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+Unstaged changes after reset:
+D	dist/images/000_mypleasure-landing.jpg
+M	dist/index.html
+M	package.json
+D	src/components/App.jsx
+M	src/components/PrivacyScreen.jsx
+M	src/components/VideoScreen.jsx
+M	src/index.jsx
+M	src/reducers/index.js
+M	webpack.config.js
---
+++ src/store/configureStore.js
@@ -0,0 +1,28 @@
+/**
+ * MyPleasure | "brand"
+ * --------------------
+ * Configure app store.
+ */
+import {createStore, compose, applyMiddleware} from 'redux';
+import reducer from '../reducers';
+import thunk from 'redux-thunk';
+
+// Create composed store with thunk middleware
+// and Redux dev tools if available on client.
+const storeWithMiddleware = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+  applyMiddleware(thunk)
+)(createStore);
+
+export default function configureStore(initialState) {
+  const store = storeWithMiddleware(reducer, initialState);
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
