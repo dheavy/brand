67fd6d0ebd39fc24215cb0f8d97e8d58b1461a7d
Davy Peter Braun <hello@davybraun.com>
2016-01-25 13:54:15 +0100

    Add CurrentScreenIndicator comp.

---
+++ src/components/CurrentScreenIndicator.jsx
@@ -0,0 +1,15 @@
+/**
+ * MyPleasure | "brand"
+ * --------------------
+ * Stateless, functional component for the current screen indicator.
+ */
+
+import React, {PropTypes} from 'react';
+
+const CurrentScreenIndicator = props => {
+  return (
+    <div>CurrentScreenIndicator</div>
+  )
+};
+
+export default CurrentScreenIndicator;
