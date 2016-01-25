ab7544022e8de59dfd966f34b8975888e7e9d1ca
Davy Peter Braun <hello@davybraun.com>
2016-01-25 13:48:58 +0100

    Remove reducer in favor of a more modular setup, and PureComponent in favor of functional stateless comps.

--- src/components/PureComponent.jsx
+++
@@ -1,9 +0,0 @@
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import React, {Component} from 'react';
-
-export default class PureComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-  }
-}
