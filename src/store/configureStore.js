627dbd411d3af15fb5494f4102878fc09307e567
Davy Peter Braun <hello@davybraun.com>
2016-01-26 11:22:40 +0100

    Update store configuration to include content from JSON.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,6 +7,7 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import reducer from '../reducers';
 import thunk from 'redux-thunk';
+import {Map} from 'immutable';
 
 // Create composed store with thunk middleware
 // and Redux dev tools if available on client.
@@ -15,8 +16,11 @@ const storeWithMiddleware = compose(
   applyMiddleware(thunk)
 )(createStore);
 
-export default function configureStore(initialState) {
-  const store = storeWithMiddleware(reducer, initialState);
+export default function configureStore(initialState = Map(), sectionsFromJSON) {
+  const state = initialState.merge(sectionsFromJSON);
+  const store = storeWithMiddleware(
+    reducer, state
+  );
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
