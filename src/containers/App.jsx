607511e634eaa5f6240758780178a0b42f02e360
Davy Peter Braun <hello@davybraun.com>
2016-01-26 15:46:06 +0100

    Add 'viewed' props.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,6 +6,7 @@
 
 import CurrentScreenIndicator from '../components/CurrentScreenIndicator';
 import ProvidersScreen from '../components/ProvidersScreen';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import PrivacyScreen from '../components/PrivacyScreen';
 import VideoScreen from '../components/VideoScreen';
 import React, {Component, PropTypes} from 'react';
@@ -19,7 +20,8 @@ function mapStateToProps(state) {
   return {
     currentSection: state.get('currentSection'),
     maxSections: state.get('maxSections'),
-    sections: state.get('sections')
+    sections: state.get('sections'),
+    viewed: state.get('viewed')
   }
 }
 
@@ -29,7 +31,16 @@ function mapDispatchToProps(dispatch) {
   }
 }
 
-function privacyScreenProps(props) {
+// Merge props to add the 'viewed' value
+// (i.e. array referencing which screens have been viewed
+// and should be removed).
+function screenProps(props, viewed) {
+  return Object.assign({}, props, {viewed})
+}
+
+// Merge props for 'Provider' screen, to transform arrays
+// of providers into immutable Lists.
+function providerScreenProps(props) {
   const providersNormal = List.of(props.providersNormal);
   const providersNaughty = List.of(props.providersNaughty);
   return Object.assign({}, props, {providersNormal, providersNaughty});
@@ -40,7 +51,8 @@ class App extends Component {
     currentSection: PropTypes.number.isRequired,
     maxSections: PropTypes.number.isRequired,
     actions: PropTypes.object.isRequired,
-    sections: PropTypes.object.isRequired
+    sections: PropTypes.object.isRequired,
+    viewed: ImmutablePropTypes.set.isRequired
   };
 
   // Debounced mousewheel user action
@@ -57,16 +69,17 @@ class App extends Component {
   render() {
     const {
       actions: {nextScreen, submitEmail},
-      sections
+      sections,
+      viewed
     } = this.props;
 
     return (
       <main>
         <section>
-          <VideoScreen {...sections.VideoScreenEnd} />
-          <ProvidersScreen {...sections.ProvidersScreen} />
-          <PrivacyScreen {...privacyScreenProps(sections.PrivacyScreen)} />
-          <VideoScreen {...sections.VideoScreenStart} />
+          <VideoScreen {...screenProps(sections.VideoScreenEnd, viewed)} />
+          <ProvidersScreen {...screenProps(providerScreenProps(sections.ProvidersScreen), viewed)} />
+          <PrivacyScreen {...screenProps(sections.PrivacyScreen, viewed)} />
+          <VideoScreen {...screenProps(sections.VideoScreenStart, viewed)} />
         </section>
         <nav>
           <CurrentScreenIndicator currentSection={this.props.currentSection} />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -20,9 +20,15 @@ import {Map} from 'immutable';
 
 export default function landingPage(state = INITIAL_STATE, action) {
   switch (action.type) {
+    // Ultimately removes current screen from view.
+    // Update `currentSection` key, preventing it from exceeding
+    // the maximum number of sections.
+    // Update `viewed`: add ID/index of screen being removed
+    // to the Set of `viewed` sections.
     case NEXT_SCREEN:
       return state.merge(Map({
-        currentSection: Math.min(action.currentSection, MAX_SECTIONS)
+        currentSection: Math.min(action.currentSection, MAX_SECTIONS),
+        viewed: state.get('viewed').add(action.currentSection - 1)
       }));
 
     default:
