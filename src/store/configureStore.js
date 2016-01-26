7098f7e810c2654c44f8480ccbc847a82bec3fce
Davy Peter Braun <hello@davybraun.com>
2016-01-26 15:45:23 +0100

    Remove some comments and reduce code footprint.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -9,18 +9,13 @@ import reducer from '../reducers';
 import thunk from 'redux-thunk';
 import {Map} from 'immutable';
 
-// Create composed store with thunk middleware
-// and Redux dev tools if available on client.
 const storeWithMiddleware = compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f,
   applyMiddleware(thunk)
 )(createStore);
 
-export default function configureStore(initialState = Map(), sectionsFromJSON) {
-  const state = initialState.merge(sectionsFromJSON);
-  const store = storeWithMiddleware(
-    reducer, state
-  );
+export default function configureStore(initialState = null, sectionsFromJSON) {
+  const store = storeWithMiddleware(reducer, initialState.merge(sectionsFromJSON));
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
