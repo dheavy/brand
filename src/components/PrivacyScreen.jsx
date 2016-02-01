a99d1514ef93e084f5f0dffa38c5bffb2eca9559
Davy Peter Braun <hello@davybraun.com>
2016-02-01 17:36:07 +0100

    Integration of PrivacyScreen on desktop.

--- src/components/PrivacyScreen.jsx
+++ src/components/PrivacyScreen.jsx
@@ -9,6 +9,17 @@ import React, {PropTypes} from 'react';
 import Form from './Form';
 import Logo from './Logo';
 
+// Compute style for background feed image.
+// Decide whether or not image is visible based on a simple condition:
+// has the preceding screen been viewed and disappeared already?
+const feedStyle = (id, viewed) => {
+  let style = 'feed';
+  if (viewed.contains(--id)) {
+    style += ' is-visible';
+  }
+  return style;
+}
+
 const PrivacyScreen = props => {
   return (
     <section className="screen privacy" style={props.style}>
@@ -23,7 +34,9 @@ const PrivacyScreen = props => {
             <Form classNames={"column small-15 medium-15 large-15"} cta={props.cta} />
           </div>
         </div>
-        <div className="column small-x medium-10 large-10">o</div>
+        <div className="column small-x medium-10 large-10">
+          <div className={feedStyle(props.id, props.viewed)}></div>
+        </div>
       </div>
     </section>
   );
