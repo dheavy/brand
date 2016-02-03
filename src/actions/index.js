595cde83f6a677c64c60ea27cdc4ceba9061e273
Davy Peter Braun <hello@davybraun.com>
2016-02-04 00:07:15 +0100

    Remove obsolete action, add new for input value monitoring.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,19 @@
-import * as types from '../constants/ActionTypes';
+import {
+  NEXT_SCREEN,
+  RESIZE,
+  UPDATE_INPUT_VALUE
+} from '../constants/ActionTypes';
 
-export function nextScreen(section) {
-  return {type: types.NEXT_SCREEN, currentSection: ++section };
+export submitForm from './form';
+
+export function updateInputValue(newValue) {
+  return {type: UPDATE_INPUT_VALUE, inputValue: newValue};
 }
 
-export function submitEmail() {
-  return {type: types.SUBMIT_EMAIL};
+export function nextScreen(section) {
+  return {type: NEXT_SCREEN, currentSection: ++section};
 }
 
 export function resize({width, height}) {
-  return {type: types.RESIZE, width: width, height: height};
+  return {type: RESIZE, width: width, height: height};
 }
