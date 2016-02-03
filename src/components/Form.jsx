2b0c8c6f670651616be802e1af7284f374c529b5
Davy Peter Braun <hello@davybraun.com>
2016-02-04 00:08:15 +0100

    Implement behavior on form component.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,6 @@
 /**
  * MyPleasure | "brand"
  * --------------------
- * Stateless, functional component for the form.
- */
-
-/**
- * MyPleasure | "brand"
- * --------------------
  * Stateless, functional form component.
  */
 
@@ -21,9 +15,28 @@ const Form = props => {
       action={props.formData.action}
     >
       <div className="input-group">
-        <input className="input-group-field" type="email" placeholder="Email" />
+        <input
+          className="input-group-field"
+          type="email"
+          placeholder="Email"
+          defaultValue={props.inputValue}
+          onChange={props.inputChange}
+        />
         <div className="input-group-button small-20">
-          <input name={props.formData.inputName} type="submit" className="button" value={props.cta} />
+          <input
+            name={props.formData.inputName}
+            type="submit"
+            className="button"
+            value={props.cta}
+            onClick={(e) => {
+              e.preventDefault();
+              props.submit({
+                email: props.inputValue,
+                formAction: props.formData.action,
+                formName: props.formName
+              });
+            }}
+          />
         </div>
       </div>
     </form>
@@ -34,7 +47,10 @@ Form.propTypes = {
   formName: PropTypes.string.isRequired,
   formData: PropTypes.object.isRequired,
   classNames: PropTypes.string.isRequired,
-  cta: PropTypes.string.isRequired
+  cta: PropTypes.string.isRequired,
+  inputChange: PropTypes.func.isRequired,
+  submit: PropTypes.func.isRequired,
+  inputValue: PropTypes.string
 };
 
 export default Form;
