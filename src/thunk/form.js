ebc186f10f92dd739c74cc2efda958d51b6a92e1
Davy Peter Braun <hello@davybraun.com>
2016-02-10 23:32:31 +0100

    Forms actions become thunk.

---
+++ src/thunk/form.js
@@ -0,0 +1,36 @@
+import fetchJsonp from 'fetch-jsonp';
+
+import {
+  FORM_REQUEST_BEGIN,
+  FORM_REQUEST_END
+} from '../constants/ActionTypes';
+
+function formRequestBegin({formName}) {
+  return {
+    type: FORM_REQUEST_BEGIN,
+    form: formName
+  };
+}
+
+function formRequestEnd({formName, response}) {
+  return {
+    type: FORM_REQUEST_END,
+    formName: formName,
+    result: response.result,
+    msg: response.msg
+  };
+}
+
+export default function submitForm({email, formAction, formName}) {
+  return dispatch => {
+    dispatch(formRequestBegin({formName}));
+
+    return fetchJsonp(formAction, {method: 'GET'})
+      .then(response => response.json())
+      .then(response => {
+        console.log('response', response);
+        dispatch(formRequestEnd({formName, response}));
+      })
+      .catch(error => console.log(error))
+  };
+}
